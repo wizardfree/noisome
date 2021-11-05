9dcb134e601e8c99c31c1ec4bcec645a0809899d
Joseph Fleet <JosephSFleet@gmail.com>
2021-11-05 12:38:26 +0000

    Added Tailwind
    Tailwind CSS added to project, initialisation of package.json, and postcss/tailwind config files.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: {
+    enabled: process.env.HUGO_ENVIRONMENT === "production",
+    content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  important: true,
+}
